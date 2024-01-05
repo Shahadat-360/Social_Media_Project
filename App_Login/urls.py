696522d873d1fb3baad7c8b9617986bcb6e0e69a
MD.Shahadat <md_shahadat_@outlook.com>
2024-01-05 11:36:39 +0600

    create login page

--- App_Login/urls.py
+++ App_Login/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'App_Login'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='sign_up'),
+    path('login/', views.login_page, name='login')
 ]
--- App_Login/views.py
+++ App_Login/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, HttpResponseRedirect
 from App_Login.forms import CreateNewUser
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy, reverse
-import App_Login
+from App_Login.models import UserProfile
 
 # Create your views here.
 
@@ -15,5 +16,19 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             registered = True
-            pass
+            user_profile=UserProfile(user=user)
+            return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/signup.html', context={'title': 'Sign Up MakeIt', 'form': form})
+
+def login_page(request):
+    form=AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password =form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                pass
+    return render(request,'App_Login/login.html',context={'title': 'Login','form':form})
