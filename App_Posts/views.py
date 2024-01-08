26de10fab4d91f66d9e9006b6a03f1c920fdb192
MD.Shahadat <md_shahadat_@outlook.com>
2024-01-08 16:21:18 +0600

    Liked and Unliked set

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, reverse
 from django.contrib.auth.decorators import login_required
 from App_Login.models import Follow
-from App_Posts.models import Posts
+from App_Posts.models import Posts, Like
 
 
 # Create your views here.
@@ -14,8 +14,29 @@ def home(request):
     result = ''
     following_list = Follow.objects.filter(follower=request.user)
     posts = Posts.objects.filter(author__in=following_list.values_list('following'))
+    liked_post = Like.objects.filter(user=request.user)
+    liked_post_list = liked_post.values_list('post', flat=True)
     if request.method == "GET":
         search = request.GET.get("search", '')
         result = User.objects.filter(username__icontains=search)
     return render(request, 'App_Posts/home.html',
-                  context={'title': 'Home', 'search': search, 'result': result, 'posts': posts})
+                  context={'title': 'Home', 'search': search, 'result': result, 'posts': posts,
+                           'liked_post_list': liked_post_list})
+
+
+@login_required
+def liked(request, pk):
+    post = Posts.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    if not already_liked:
+        liked_post = Like(post=post, user=request.user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('App_Posts:home'))
+
+
+@login_required
+def unliked(request, pk):
+    post = Posts.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('App_Posts:home'))
